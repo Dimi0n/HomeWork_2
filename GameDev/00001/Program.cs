d809a9c5ee2a2c6e2bb746149265499206d22fe5
Dimion <ardgalen@dimlev.ru>
2023-01-17 19:38:32 +0300

    delenije po modulu %

---
+++ GameDev/00001/Program.cs
@@ -0,0 +1,7 @@
+﻿int timeInMinets = 130; 
+int hours;
+int minute;
+hours = timeInMinets / 60;
+minute = timeInMinets % 60;
+System.Console.WriteLine( "hours: " + hours);
+System.Console.WriteLine("minutes: " + minute);
