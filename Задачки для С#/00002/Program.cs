d3601ee75a80979acb164088f8873cda55dde78f
Dimion <ardgalen@dimlev.ru>
2023-01-19 14:14:01 +0300

    two tasks

---
+++ Задачки для С#/00002/Program.cs
@@ -0,0 +1,10 @@
+﻿// Составить программу вывода на экран числа, вводимого с клавиатуры. 
+//Выводимому числу должно предшествовать сообщение «Вы ввели число».
+
+string chislo;
+System.Console.WriteLine("Введите число:");
+
+chislo = Console.ReadLine();
+
+System.Console.WriteLine("Вы ввели число: " + chislo);
+
