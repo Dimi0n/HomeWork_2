c5d0024ed30294e7e34e28b362a4c80c6f3c0430
Dimion <ardgalen@dimlev.ru>
2023-01-20 13:40:09 +0300

    Первое задание от Simple Code

---
+++ Задачки для С#/10000/Program.cs
@@ -0,0 +1,19 @@
+﻿// 1. Среднее арифметическое 2х чисел
+double a = 5;
+int b = 12;
+double result = (a+b)/2;
+Console.WriteLine(result);
+
+//2. Сумма и произведение 3х чисел
+
+int a1 = 5;
+int b1 = 12;
+int c1 = 33;
+System.Console.WriteLine(a1+b1+c1);
+System.Console.WriteLine((a1*b1)*c1);
+
+// 3. Примитивный обмен валют
+double USD = 1;
+double RUB = 68.5;
+double result1 = RUB/USD;
+System.Console.Write("1 доллар США равен: " + result1 + "рублей");
