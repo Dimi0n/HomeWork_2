10af02d6af7dbef22f569203226bd70fd0625259
Dimion <ardgalen@dimlev.ru>
2023-01-21 16:35:49 +0300

    finished

--- Задачки для С#/Расчет Bitzer 4666/Program.cs
+++ Задачки для С#/Расчет Bitzer 4666/Program.cs
@@ -1,15 +1,15 @@
-﻿double EXW, Conv, TransToArm, CommisionInArm, Insuranse1, SaleArmRF, Insuranse2, TransportArmSV;
-double duty, VAT, DC, Broker, FinalDDPSVEUR, FinalDDPSVRUR, EURRUB;
+﻿double EXW, Conv, TransToArm, CommisionInArm, Insuranse1, Insuranse2, TransportArmSV;
+double duty, VAT, DC, Broker, EURRUB;
 
 EURRUB = 73.11;
 
-System.Console.WriteLine("Введите значение: exw ");
+System.Console.WriteLine("Введите значение: EXW ");
 EXW = Convert.ToInt32(Console.ReadLine());
 System.Console.WriteLine("Введите значение: Конвертация в % ");
 Conv = Convert.ToInt32(Console.ReadLine());
 double result1 = (EXW*(Conv/100));
 
-System.Console.WriteLine("Введите значение: Транспорт в Армению ");
+System.Console.WriteLine("Введите значение: Транспорт в Армению EUR ");
 TransToArm = Convert.ToInt32(Console.ReadLine());
 
 System.Console.WriteLine("Введите значение: Комиссия партнера в Армении в % ");
@@ -25,5 +25,39 @@ double result4 = result3*EURRUB;
 
 System.Console.WriteLine("В Армении товар будет стоить в ЕВРО: " + result3); 
 System.Console.WriteLine("В Рублях: " + result4);
+
+Insuranse2 = (result3*0.002);
+
+System.Console.WriteLine("Введите значение: Транспорт Армения - Севастополь EUR ");
+TransportArmSV = Convert.ToInt32(Console.ReadLine());
+
+System.Console.WriteLine("Введите значение: Пошлина в % ");
+duty = Convert.ToInt32(Console.ReadLine());
+double result5 = (Insuranse2+result3);
+double result6 = result5*(duty/100);
+System.Console.WriteLine("Пошлина в EUR = " + result6);
+
+System.Console.WriteLine("Введите значение: НДС в % ");
+VAT = Convert.ToInt32(Console.ReadLine());
+double result7 = (result5 + duty);
+double result8 = result7*(VAT/100);
+System.Console.WriteLine("НДС в EUR = " + result8);
+
+DC = 341.95;
+Broker = 683.9;
+
+double result9 = (result3 + result6 + result8 + DC + VAT + TransportArmSV + Broker);
+double result10 = result9*EURRUB;
+double result11 = (result9/EXW);
+
+System.Console.WriteLine("Итого DDP Севастополь в ЕВРО: " + result9); 
+System.Console.WriteLine("Итого DDP Севастополь в Рублях: " + result10);
+System.Console.WriteLine("Коэфицент DDP " + result11);
+
+
+
+
+
+
 
 
