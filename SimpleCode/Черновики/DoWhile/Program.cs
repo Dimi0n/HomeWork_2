345b903eccd92cde55301b7ba30a34e265c6c392
Dimion <ardgalen@dimlev.ru>
2023-01-24 16:00:42 +0300

    sec, smotrju if

---
+++ SimpleCode/Черновики/DoWhile/Program.cs
@@ -0,0 +1,12 @@
+﻿int count = 0;
+int limit = int.Parse(Console.ReadLine());
+while (count < limit)
+{
+    count++;
+    System.Console.WriteLine(count);
+
+    if (count/2 == 0)
+    {
+        System.Console.WriteLine(summ(count));
+    }
+}
