8aa11ecb787e873cb94b7ae869cd0e99559c9aa4
Dimion <ardgalen@dimlev.ru>
2023-01-25 09:43:35 +0300

    dowhile

--- SimpleCode/Черновики/DoWhile/Program.cs
+++ SimpleCode/Черновики/DoWhile/Program.cs
@@ -2,11 +2,9 @@
 int limit = int.Parse(Console.ReadLine());
 while (count < limit)
 {
+    count = (count%2 == 0);
     count++;
-    System.Console.WriteLine(count);
+    Console.WriteLine(count);
 
-    if (count/2 == 0)
-    {
-        System.Console.WriteLine(summ(count));
-    }
+    
 }
